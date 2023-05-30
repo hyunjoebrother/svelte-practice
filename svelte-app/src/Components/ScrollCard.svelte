f5e6e70c269af846d03848bf6643923c88f6755d
hyunjoebrother <hyunjoe31@gmail.com>
2023-05-30 18:31:55 +0900

    feat: 수평 스크롤 캐러셀 컨테이너 컴포넌트 개발

---
+++ svelte-app/src/Components/ScrollCard.svelte
@@ -0,0 +1,55 @@
+<script>
+    import Profilecard from "./Profilecard.svelte";
+</script>
+
+<p>수평 방향으로 카드 컴포넌트 스크롤 캐러셀 컨테이너 컴포넌트</p>
+
+<div class="container">
+      <div class="wrapper">
+          <div class="title">컨테이너 제목</div>
+          <div class="info">내용내용</div>
+          <div class="card-scroll-container">
+              <!-- 카드 컴포넌트 -->
+              <Profilecard />
+              <Profilecard />
+              <Profilecard />
+              <Profilecard />
+              <Profilecard />
+        </div>
+    </div>
+</div>
+  
+  
+  <style>
+      .container {
+          width: 100%;
+          height: 228px;
+          margin-top: 10px;
+          box-shadow: 0 4px 4px -4px;
+      }
+  
+      .wrapper {
+          padding: 16px;
+      }
+  
+      .title {
+          display: flex;
+          margin-left: 16px;
+          font-size: 16px;
+          font-weight: 500;
+      }
+  
+      .info {
+          margin-right: 60px;
+          font-size: 14px;
+          text-align: right;
+      }
+  
+      .card-scroll-container {
+          margin: 10px 0;
+          display: flex;
+          overflow-x: scroll;
+          overflow-y: hidden;
+          flex-wrap: nowrap;
+      }
+  </style>
