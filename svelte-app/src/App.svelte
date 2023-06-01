8a04caa7d5dd2e790a87bf6327c47eb3a24732b5
hyunjoebrother <hyunjoe31@gmail.com>
2023-06-01 23:29:51 +0900

    feat: Login Page UI Component

--- svelte-app/src/App.svelte
+++ svelte-app/src/App.svelte
@@ -3,6 +3,7 @@
 	import MainPage from "./routes/MainPage.svelte";
   	import HomePage from "./routes/HomePage.svelte";
 	import Footer from "./Components/Footer.svelte";
+  import LoginPage from "./routes/LoginPage.svelte";
 
 	export let url="";
 </script>
@@ -11,10 +12,12 @@
 	<nav>
 		<Link to="/">메인</Link>
 		<Link to="/home">홈</Link>
+		<Link to="/login">로그인</Link>
 	</nav>
 
 	<Route path="/" component={MainPage} />
 	<Route path="/home" component={HomePage} />
+	<Route path="/login" component={LoginPage}/>
 </Router>
 
 <Footer />
