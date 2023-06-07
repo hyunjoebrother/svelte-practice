a488c5ee0e33c130d14537c909dbb43df7aac13a
hyunjoebrother <hyunjoe31@gmail.com>
2023-06-07 15:41:37 +0900

    feat: Routing Join Page by navigate

--- svelte-app/src/App.svelte
+++ svelte-app/src/App.svelte
@@ -4,6 +4,10 @@
   	import HomePage from "./routes/HomePage.svelte";
 	import Footer from "./components/Footer.svelte";
 	import LoginPage from "./routes/LoginPage.svelte";
+  import JoinStep1 from "./routes/JoinPage/JoinStep1.svelte";
+  import JoinStep2 from "./routes/JoinPage/JoinStep2.svelte";
+  import JoinStep3 from "./routes/JoinPage/JoinStep3.svelte";
+  import JoinFinish from "./routes/JoinPage/JoinFinish.svelte";
 
 	export let url="";
 </script>
@@ -21,6 +25,11 @@
 	<Route path="/" component={MainPage} />
 	<Route path="/home" component={HomePage} />
 	<Route path="/login" component={LoginPage}/>
+
+	<Route path="/join" component={JoinStep1} />
+	<Route path="/join/step2" component={JoinStep2} />
+	<Route path="/join/step3" component={JoinStep3} />
+	<Route path="/join/finish" component={JoinFinish} />
 </Router>
 
 <Footer />
---
+++ svelte-app/src/routes/JoinPage/JoinStep2.svelte
@@ -0,0 +1,16 @@
+<script>
+    import { navigate } from "svelte-routing";
+  
+    const handleNext = () => {
+      // 유효성 검사 등 필요한 로직 수행
+  
+      // 다음 페이지로 이동
+      navigate("/join/step3");
+    };
+  </script>
+  
+  <main>
+    <h1>회원가입 프로세스 스텝 2</h1>
+        <button on:click={handleNext}>다음</button>
+  </main>
+  
